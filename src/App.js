e1039c674a30a407932177bb7a85b9f29a7c428d
Abdoul Hakim Billa <abdoulhakimbilla30@outlook.com>
2023-05-31 07:11:06 +0200

    routing is now working

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import './App.css';
 import Board from './pages/Board/Board';
 import Header from './components/Header/Header';
 import Backlog from './pages/Backlog/Backlog';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className='App'>
-      <Header />
-      <Board />
-      <Backlog />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Board />} />
+          <Route path="/backlog" element={ <Backlog />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,16 @@
-import './Header.css'
+import "./Header.css";
+import { Link as RouterLink } from "react-router-dom";
 
 function Header() {
   return (
     <div className="Header-wrapper">
       <h1>Project Management Board</h1>
+      <nav>
+        <RouterLink to="/" className="Nav-link">Board</RouterLink>
+        <RouterLink to="/backlog" className="Nav-link">Backlog</RouterLink>
+      </nav>
     </div>
-  )
+  );
 }
 
 export default Header;
